89c9f042b015321373764842e7e311d9f43f02f6
tapnapong wongkaisron <tapanpong.w@ku.th>
2025-09-25 15:19:53 +0700

    work

--- assignments/week10/personal-validator.py
+++ assignments/week10/personal-validator.py
@@ -28,3 +28,52 @@ Age Group: Young Adult (18-30)
 Phone: +91-9876543210
 
 """
+
+print("PERSONAL INFORMATION VALIDATOR")
+Name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+phone = input("Enter your phonenumber: ")
+Name_validator = True
+for char in Name:
+    if char.isalpha():
+        Name_validator = True
+    else:
+        Name_validator = False
+age_validator = True
+if 18<=age and age<=65:
+    age_validator = True
+else:
+    age_validator = False
+
+if len(phone)!=10:
+    print("Error")
+phone_validator = True
+for char in phone:
+    if char.isdigit():
+        phone_validator = True
+    else:
+        phone_validator = False
+
+print("Validation Results")
+if Name_validator == True:
+    print("Name: Valid (contains only letters and spaces)")
+else:
+    print("Name: Invalid")
+if age_validator == True:
+    print(f"age: Valid({age} years old)")
+else:
+    print("age: Invalid")
+if phone_validator == True:
+    print("phone:Valid (10-digit number)")
+else:
+    print("phone:Invalid")
+
+print("Formatted Information")
+if Name_validator == True:
+    print(f"Name: {Name.upper()}")
+
+if age_validator == True:
+    print("Age Group: Young Adult (18-30)")
+
+if phone_validator == True:
+    print(f"phone: +66-{phone[1:]}")
